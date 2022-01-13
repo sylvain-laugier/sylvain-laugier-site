4872ba9427b7708434d9dc8b0a0ada4845136d45
Sylvain Laugier <sylvain.laugier.net@gmail.com>
2022-01-13 10:19:03 +0100

    moved components to 'shared' subfolder

---
+++ src/components/shared/TitleIntro.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function TitleIntro() {
+	return (
+		<div className="title-intro-container">
+			<h1>
+				<span className="first-name">Sylvain</span>{' '}
+				<span className="last-name">Laugier</span>
+			</h1>
+			<h2>Creative web developper</h2>
+		</div>
+	);
+}
